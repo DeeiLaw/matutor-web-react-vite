ba52a5edb5ff0d3b8664c2591ceabe29a4e7666f
Russelle Dianon <ytheshibe@gmail.com>
2023-11-17 22:27:08 +0800

    Revamped register and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
+import Dashboard from './Components/SignUp/SignUp';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
 
--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,46 +1,286 @@
-import React, { useState } from 'react'
-import './LoginSignup.css'
+import React, { useState } from "react";
+import "./LoginSignup.css";
+import axios from "axios";
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
-//import { AiFillHome } from "react-icons/ai" <AiFillHome/>
+import user_icon from "../Assets/person.png";
+import email_icon from "../Assets/email.png";
+import password_icon from "../Assets/password.png";
+import { AiFillHome } from "react-icons/ai";
+import { MdCake } from "react-icons/md";
+import { RiParentFill } from "react-icons/ri";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { FaSchoolCircleCheck } from "react-icons/fa6";
 
 const LoginSignup = () => {
 
-    const [action, setAction] = useState("Login");
+  const [action, setAction] = useState("Login");
+  const [item, setItem] = useState("Learner");
 
-    return (
-        <div className='container'>
-            <div className="header">
-                <div className="text">{action}</div>
-                <div className="underline"></div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const learnerUrl = "http://localhost:3000/learner/login";
+
+  const handleLoginClick = (event) => {
+    try {
+      axios
+        .post(learnerUrl, {
+          learnerEmail: email,
+          learnerPassword: password,
+        })
+        .then((response) => {
+          const responseData = response.data;
+          console.log(responseData);
+
+          sessionStorage.setItem("userData", JSON.stringify(responseData));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (Exception) {
+      console.error(Exception);
+    }
+    setAction("Login");
+  };
+
+  const handleSignUpClick = (event) => {
+    try {
+      axios.post(learnerUrl);
+    } catch (event) {}
+    setAction("Sign Up");
+  };
+
+  /*function userType(event) {
+        console.log(event.target.value)
+    } get*/
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+        {action === "Login" ? (
+          /* RENDER LOGIN FIELDS*/
+          <div className="inputs">
+            <div className="input">
+              <img src={email_icon} alt="" />
+              <input type="email" placeholder="Email nga orig" onChange={(e) => { setEmail(e.target.value); }}/>
             </div>
-            <div className="inputs"> 
-                {action==="Login"?<div></div>:<div className="input">
-                    <img src={user_icon} alt="" />
-                    <input type="text" placeholder="Name"/>
-                </div>}
-                <div className="input">
-                    <img src={email_icon} alt="" />
-                    <input type="email" placeholder="Email ID"/>
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="password" placeholder="Password"/>
-                </div>
-                <div className="input">
-                    <img src={password_icon} alt="" />
-                    <input type="text" placeholder="Address"/>
-                </div>
+            <div className="input">
+              <img src={password_icon} alt="" />
+                <input type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value);}}/>
             </div>
-            {action==="Sign Up"?<div></div>: <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
-            <div className="submit-container">
-                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-                <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+          </div>
+        ) : (
+          /* Render Register Fields */
+          <div className="inputs">
+            <select className="selection" value={item} onChange={(e) => setItem(e.target.value)}>
+              <option value="Learner">Learner</option>
+              <option value="Tutor">Tutor</option>
+              <option value="Tutoring Center">Tutoring Center</option>
+            </select>
+            
+            <div className="input">
+              <img src={email_icon} alt="" />
+              <input type="email" placeholder="Email" />
             </div>
+
+            {item === "Tutoring Center" ? (
+              <div className="input">
+                <FaSchoolCircleCheck  className="inputIcon" size="1.7em" alt="AddressIcon" />
+                <input type="text" placeholder="Tutor Center's Name" />
+              </div>
+            ) : (
+            <div>
+              <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder="First Name" />
+              </div>
+              <br/>
+              <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder="Last Name" />
+              </div>
+            </div> 
+            )}
+
+            {item === "Learner" ? ( 
+              <div>
+                <div className="input">
+                  <RiParentFill className="inputIcon" size="1.7em" alt="AddressIcon" />
+                  <input type="email" placeholder="Parent's/Guardian's Email" />
+                </div>
+                <br/>
+                <div className="input">
+                  <RiParentFill className="inputIcon" size="1.7em" alt="AddressIcon" />
+                  <input type="text" placeholder="Guardian's Full Name" />
+                </div>
+              </div>
+            ):(<div hidden></div> )}
+
+            <div className="input">
+              <BsFillTelephoneFill className="inputIcon" size="1.7em" alt="AddressIcon" />
+              <span className="spanPhone">+63</span><input className="inputPhone" type="text" placeholder="Contact Number" />
+            </div>
+            {item === "Tutor" ? ( 
+              <div className="input">    
+                <FaSchoolCircleCheck  className="inputIcon" size="1.7em" alt="AddressIcon" />
+                <input className="inputPhone" type="text" placeholder="Associated Tutoring Center" />
+              </div>
+            ):(<div hidden></div>)}
+
+            <div className="input">
+              <AiFillHome className="inputIcon" size="1.7em" alt="AddressIcon" />
+              <input type="text" placeholder="Address" />
+            </div>
+
+            {item === "Tutoring Center" ? ( 
+              <div hidden></div>                 
+            ) : (
+              <div className="input">
+                <MdCake className="inputIcon" size="1.7em" alt="Birthdate" />
+                <input type="date" placeholder="Birthdate" />
+              </div>
+            )}
+            
+            <div className="input">
+            <img src={password_icon} alt="" />
+                <input type="password" placeholder="Password"/>
+            </div>
+
+          </div>
+        )}
+      {action === "Sign Up" ? (
+        <div hidden></div>
+      ) : (
+        <div className="forgot-password">
+          Forgot Password? <span>Click Here!</span>
         </div>
-    )
-}
+      )}
+      <div className="submit-container">
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={handleSignUpClick}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={handleLoginClick}
+        >
+          Login
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
+
+/* 
+<div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? (
+          <div hidden></div>
+        ) : (
+          <select
+            className="selection"
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          >
+            <option value="Learner">Learner</option>
+            <option value="Tutor">Tutor</option>
+            <option value="Tutoring Center">Tutoring Center</option>
+          </select>
+        )}
+        {action === "Login" ? (
+          <div hidden></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
+        {action === "Sign Up" && item === "Learner" ? (
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Your Email" />
+          </div>
+        ) : (
+          <div hidden></div>
+        )}
+        <div className="input">
+          <img src={email_icon} alt="" />
+          {action === "Login" ? (
+            <input
+              type="email"
+              placeholder="Email nga orig"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          ) : (
+            <div>
+              {{
+                Learner: (
+                  <input type="email" placeholder="Parent's/Guardian's Email" />
+                ),
+              }[item] || <input type="email" placeholder="Email" />}
+            </div>
+          )}
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <AiFillHome className="inputIcon" size="1.7em" alt="AddressIcon" />
+            <input type="text" placeholder="Address" />
+          </div>
+        )}
+        {action === "Sign Up" && item !== "Tutoring Center" ? (
+          <div className="input">
+            <MdCake className="inputIcon" size="1.7em" alt="Birthdate" />
+            <input type="date" placeholder="Birthdate" />
+          </div>
+        ) : (
+          <div hidden></div>
+        )}
+      </div>
+      {action === "Sign Up" ? (
+        <div hidden></div>
+      ) : (
+        <div className="forgot-password">
+          Forgot Password? <span>Click Here!</span>
+        </div>
+      )}
+      <div className="submit-container">
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={handleSignUpClick}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={handleLoginClick}
+        >
+          Login
+        </div>
+      </div>
+    </div>
+
+*/
