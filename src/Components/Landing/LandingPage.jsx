450fd5402efe8ce5aff404fe5a4d4ffb4cbabcdd
Russelle Dianon <ytheshibe@gmail.com>
2023-12-04 16:37:18 +0800

    Added Landing Page

--- src/Components/Landing/LandingPage.jsx
+++ src/Components/Landing/LandingPage.jsx
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 import "./Landing.css";
 import whatImg from "../Assets/what is matutor.png";
 import studentsLP from "../Assets/studentsLP.png";
+import { LuMousePointerClick } from "react-icons/lu";
+
 
 const HomePage = () => {
 
     return (
       <div>
         <div className="box1">
+        <LuMousePointerClick className="pointer"/>
           <img src={studentsLP} alt="" className="studentsLP"/>
           <img src={whatImg} alt="" className="whatIMG"/>
           <div className="paragraph">
@@ -17,7 +20,7 @@ const HomePage = () => {
           <Link to="/login"
                 className="button-submit"
               >
-                start now
+                ready to join us?
             </Link>      
         </div>
       </div>
